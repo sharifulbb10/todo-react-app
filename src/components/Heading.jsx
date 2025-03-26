64e7c590043e6059611e4ebd12a185511dbadbf6
sharifulbb10 <sharifulbb10@gmail.com>
2025-03-27 03:32:52 +0600

    no offense

--- src/components/Heading.jsx
+++ src/components/Heading.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Heading() {
 	return (
 		<div>
-			<div className="m-2 w-auto h-25 bg-[url(/src/assets/bg.jpeg)] flex items-center justify-center">
+			<div className="m-2 w-auto h-25 bg-[url(/src/assets/bg.jpg)] flex items-center justify-center">
 				<span className="text-3xl font-bold text-white opacity-70">Todo App</span>
 			</div>
 		</div>
